cbc726dc7546ca675938fe62c043fcafcc951bfa
Manav Nanwani <manav.nanwani@gmail.com>
2021-08-28 18:58:14 +0530

    map added

--- server/controllers/classes.js
+++ server/controllers/classes.js
@@ -1,6 +1,11 @@
 import Classes from "../models/classes.js";
 import Student from "../models/student.js";
 
+export const getAllCourses = async (req, res) => {
+  const searchResults = await Classes.find();
+  return res.status(200).json({ searchResults });
+};
+
 export const getCourse = async (req, res) => {
   const { id: courseCode } = req.params;
   const searchResults = await Classes.find({ courseCode: courseCode });
